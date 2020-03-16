f9b261fa01291be0b6dc8a72fc6f7ea9892a4d10
smajdee.h@gmail.com <majdeZOA>
2020-03-16 21:43:32 +0200

    Nth samllest number

---
+++ Questions/NthSmallest.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq;
+
+namespace Questions
+{
+    public class NthSmallest
+    {
+        public int FindNthSmallest_1(int[] numbers, int nth)
+        {
+            if (numbers == null || numbers.Length < nth)
+                throw new Exception();
+
+            Array.Sort(numbers);
+
+            return numbers[nth-1];
+        }
+
+        public int FindNthSmallest_2(int[] numbers, int nth)
+        {
+            if (numbers == null || numbers.Length < nth)
+                throw new Exception();
+
+            var index = 0;
+            var startIndex = 0;
+            var endIndex = numbers.Length - 1;
+
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > numbers[index])
+                {
+                    SwapNumbers(numbers, index, endIndex);
+                    endIndex--;
+                }
+                else
+                {
+                    startIndex++;
+                }
+            }
+
+            if (startIndex < nth)
+            {
+                return FindNthSmallest_2(numbers.Skip(startIndex).ToArray(), nth - startIndex);
+            }
+
+            return FindNthSmallest_2(numbers.Take(startIndex).ToArray(), nth);
+
+        }
+
+        private void SwapNumbers(int[] numbers, int i, int index)
+        {
+            var temp = numbers[i];
+            numbers[i] = numbers[index];
+            numbers[index] = temp;
+        }
+
+        public int FindMin(int[] numbers)
+        {
+            if (numbers == null || numbers.Length < 1)
+                throw new Exception();
+
+            var min = numbers[0];
+
+            foreach (var number in numbers)
+            {
+                if (number < min)
+                {
+                    min = number;
+                }
+            }
+
+            return min;
+        }
+
+        public int FindSecondMin(int[] numbers)
+        {
+            if (numbers == null || numbers.Length < 2)
+                throw new Exception();
+
+            var firstMin = numbers[0];
+            var secondMin = numbers[1];
+
+            foreach (var number in numbers)
+            {
+                if (number < firstMin)
+                {
+                    secondMin = firstMin;
+                    firstMin = number;
+                }
+                else if (number < secondMin)
+                {
+                    secondMin = number;
+                }
+            }
+
+            return secondMin;
+        }
+    }
+}
---
+++ QuestionsTests/NthSmallestTests.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+using Questions;
+using System;
+using System.Linq;
+
+namespace QuestionsTests
+{
+    public class NthSmallestTests
+    {
+        readonly NthSmallest _nthSmallest = new NthSmallest();
+
+        [Test]
+        public void FindMin_GivenNumbers_ShouldReturnTheMin()
+        {
+            // Arrange
+            var numbers = new int[] {10, 5, 15, 1, 20};
+
+            // Act
+            var actualMin = _nthSmallest.FindMin(numbers);
+
+            // Assert
+            var expectedMin = numbers.Min();
+            Assert.AreEqual(expectedMin,actualMin);
+        }
+
+        [Test]
+        public void FindSecondMin_GivenNumbers_ShouldReturnTheSecondMin()
+        {
+            // Arrange
+            var numbers = new int[] { 10, 5, 15, 1, 20 };
+
+            // Act
+            var actualMin = _nthSmallest.FindSecondMin(numbers);
+
+            // Assert
+            Array.Sort(numbers);
+
+            var expectedSecondMin =  numbers[1];
+            Assert.AreEqual(expectedSecondMin, actualMin);
+        }
+
+        [Test]
+        public void FindNthSmallest_1_GivenNumbers_ShouldReturnTheThirdMin()
+        {
+            // Arrange
+            var numbers = new int[] { 10, 5, 15, 1, 20 };
+
+            // Act
+            var actualMin = _nthSmallest.FindNthSmallest_1(numbers,3);
+
+            // Assert
+            Array.Sort(numbers);
+
+            var expectedSecondMin = numbers[2];
+            Assert.AreEqual(expectedSecondMin, actualMin);
+        }
+
+        [Test]
+        public void FindNthSmallest_2_GivenNumbers_ShouldReturnTheThirdMin()
+        {
+            // Arrange
+            var numbers = new int[] { 10, 5, 15, 1, 20 };
+
+            // Act
+            var actualMin = _nthSmallest.FindNthSmallest_2(numbers, 3);
+
+            // Assert
+            Array.Sort(numbers);
+
+            var expectedSecondMin = numbers[2];
+            Assert.AreEqual(expectedSecondMin, actualMin);
+        }
+
+
+    }
+}
